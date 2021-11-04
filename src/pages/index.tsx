44c48e0c1bcafc667cdfc67b4d211b10ae663dee
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2021-11-03 22:08:27 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
-import styles from '../styles/home.module.css'
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>relo word</h1>
+    <>
+      <Head>
+        <title>Home | bnews</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about <span>*something*</span></h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+
+        <img src="/images/avatar.png" alt="Girl Coding" height="500px" style={{marginRight: "2rem"}}/>
+      
+      </main>
+    </>
+    
   );
 }
